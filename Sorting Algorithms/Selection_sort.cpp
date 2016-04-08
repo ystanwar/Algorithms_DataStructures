b295350a11416b7f1934c3997443d712fbde63dd
creativenikhil <creativenikhil20@gmail.com>
2016-04-08 06:08:41 +0530

    bub_,ins_ and sel_sort completed.

--- Sorting Algorithms/Selection_sort.cpp
+++ Sorting Algorithms/Selection_sort.cpp
@@ -1,5 +1,6 @@
 /**
-  Selection sort : 
+  Selection sort : This involves the comparison of ith element to all elements till n and then swaping if smaller
+  In worst case it may requre n*(n-1) comparsions leading to O(N^2) time complexity
 **/
 
 #include<iostream>
@@ -17,4 +18,23 @@ int main(){
 	Selection_sort(arrf,5);
 return 0;
 }
+
+template<typename T>void Selection_sort(T arr[],int n){
+	T temp;
+	for(int i=0;i<n-1;i++){
+		for(int j=i+1;j<n;j++){
+			if(arr[i]>arr[j]){
+				temp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+	cout<<"Sorted array is : "<<endl;
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+
 
